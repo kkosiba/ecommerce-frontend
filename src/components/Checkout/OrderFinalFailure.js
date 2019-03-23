1bce5410844447adeb2708e78b7af9cda01de0d4
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-03-23 12:06:52 +0000

    CHANGELOG
    1. Added mapDispatchToProps
    2. Added componentWillUnmount LCM

--- src/components/Checkout/OrderFinalFailure.js
+++ src/components/Checkout/OrderFinalFailure.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import {
+  setPayment,
+  toggleCheckoutComplete
+} from "../../store/actions/storeActions";
 
 const mapStateToProps = state => {
   return state.store;
 };
 
+const mapDispatchToProps = dispatch => {
+  return {
+    setPayment: value => dispatch(setPayment(value)),
+    toggleCheckoutComplete: () => dispatch(toggleCheckoutComplete())
+  };
+};
+
 class OrderFinalFailure extends Component {
+  componentWillUnmount() {
+    if (this.props.isCheckoutComplete) this.props.toggleCheckoutComplete();
+    this.props.setPayment("");
+  }
+
   render() {
     return (
       <div className="d-flex flex-column align-items-center justify-content-center">
@@ -18,4 +34,7 @@ class OrderFinalFailure extends Component {
   }
 }
 
-export default connect(mapStateToProps)(OrderFinalFailure);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(OrderFinalFailure);
