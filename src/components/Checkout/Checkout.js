eb4d5903f96fb35a4ef772ce902e0fabf31c502e
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-03-24 20:10:35 +0000

    Commented out Prompt import

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Prompt } from "react-router-dom";
+// import { Prompt } from "react-router-dom";
 import { connect } from "react-redux";
 import {
   setPayment,
