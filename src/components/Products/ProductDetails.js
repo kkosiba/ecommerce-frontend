08eae0c61825297d1119fbade7e5859e9bfda78f
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-03-26 12:20:25 +0000

    CHANGELOG
    Product details page now properly displays spinner instead of 'Page not found' while loading data
    from the backend

--- src/components/Products/ProductDetails.js
+++ src/components/Products/ProductDetails.js
@@ -10,6 +10,7 @@ import {
 import { API_PATH } from "../../backend_url";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Default from "../Misc/Default";
+import { Spinner } from "reactstrap";
 import "./css/products.css";
 
 const mapStateToProps = state => {
@@ -28,7 +29,8 @@ class ProductDetails extends Component {
     super(props);
     this.state = {
       product: {},
-      productExists: false,
+      loading: true,
+      error: null,
       selectValue: 1
     };
     this.handleChange = this.handleChange.bind(this);
@@ -40,8 +42,8 @@ class ProductDetails extends Component {
     } = this.props;
     axios
       .get(`${API_PATH}products/${params.slug}`)
-      .then(res => this.setState({ product: res.data, productExists: true }))
-      .catch(err => undefined); // ignore error
+      .then(res => this.setState({ product: res.data, loading: false }))
+      .catch(err => this.setState({ error: err, loading: false }));
   }
 
   inCart = () => {
@@ -73,100 +75,110 @@ class ProductDetails extends Component {
   };
 
   render() {
-    const { product, productExists } = this.state;
+    const { product, loading, error } = this.state;
 
-    if (!productExists) {
+    if (error) {
       return <Default />;
     } else {
-      const productQuantityRange = this.quantityRange(product);
+      if (loading) {
+        return (
+          <Spinner
+            color="secondary"
+            style={{ width: "3rem", height: "3rem" }}
+            className="m-auto"
+          />
+        );
+      } else {
+        const productQuantityRange = this.quantityRange(product);
 
-      return (
-        <div className="row">
-          <div className="col-md-6">
-            <div className="card rounded-0">
-              <img
-                className="w-100 rounded-0"
-                src={product.picture}
-                alt={product.name}
-              />
+        return (
+          <div className="row">
+            <div className="col-md-6">
+              <div className="card rounded-0">
+                <img
+                  className="w-100 rounded-0"
+                  src={product.picture}
+                  alt={product.name}
+                />
+              </div>
             </div>
-          </div>
-          <div className="col-md-6">
-            <div className="card rounded-0">
-              <div className="card-body">
-                <h3 className="font-weight-bold">{product.name}</h3>
-                <p className="card-text pt-2">{product.description}</p>
-                <p className="card-text pt-2">
-                  <strong>Price:</strong> £{product.price}
-                </p>
-                <p className="card-text py-3 border-bottom">
-                  <strong>Availability:</strong>
-                  {product.quantity > 0
-                    ? product.quantity > 20
-                      ? " Plenty in stock!"
-                      : " Just a few left!"
-                    : " Out of stock"}
-                </p>
+            <div className="col-md-6">
+              <div className="card rounded-0">
+                <div className="card-body">
+                  <h3 className="font-weight-bold">{product.name}</h3>
+                  <p className="card-text pt-2">{product.description}</p>
+                  <p className="card-text pt-2">
+                    <strong>Price:</strong> £{product.price}
+                  </p>
+                  <p className="card-text py-3 border-bottom">
+                    <strong>Availability:</strong>
+                    {product.quantity > 0
+                      ? product.quantity > 20
+                        ? " Plenty in stock!"
+                        : " Just a few left!"
+                      : " Out of stock"}
+                  </p>
 
-                <div className="input-group justify-content-center">
-                  <div className="input-group-prepend">
-                    <button className="btn btn-info" type="button">
-                      <Link to="/" className="text-white">
-                        <FontAwesomeIcon icon="list-alt" /> Products
-                      </Link>
-                    </button>
-                  </div>
-                  {product.quantity === 0 ? (
-                    <div className="input-group-append">
-                      <button className="btn btn-success" disabled>
-                        <FontAwesomeIcon icon="cart-plus" />
+                  <div className="input-group justify-content-center">
+                    <div className="input-group-prepend">
+                      <button className="btn btn-info" type="button">
+                        <Link to="/" className="text-white">
+                          <FontAwesomeIcon icon="list-alt" /> Products
+                        </Link>
                       </button>
                     </div>
-                  ) : (
-                    <div className="input-group-append">
-                      <select
-                        className="form-control rounded-0"
-                        value={this.state.selectValue}
-                        onChange={this.handleChange}
-                      >
-                        {productQuantityRange
-                          .slice(0, 10) // add no more than 10 items to cart regardless of the available amount
-                          .map(item => (
-                            <option value={item} key={item}>
-                              {item}
-                            </option>
-                          ))}
-                      </select>
-                      <button
-                        type="submit"
-                        className="btn btn-success"
-                        onClick={() =>
-                          this.addMultiple(product, this.state.selectValue)
-                        }
-                      >
-                        <FontAwesomeIcon icon="cart-plus" />
-                      </button>
-                      {this.inCart() > 0 ? (
+                    {product.quantity === 0 ? (
+                      <div className="input-group-append">
+                        <button className="btn btn-success" disabled>
+                          <FontAwesomeIcon icon="cart-plus" />
+                        </button>
+                      </div>
+                    ) : (
+                      <div className="input-group-append">
+                        <select
+                          className="form-control rounded-0"
+                          value={this.state.selectValue}
+                          onChange={this.handleChange}
+                        >
+                          {productQuantityRange
+                            .slice(0, 10) // add no more than 10 items to cart regardless of the available amount
+                            .map(item => (
+                              <option value={item} key={item}>
+                                {item}
+                              </option>
+                            ))}
+                        </select>
                         <button
-                          type="button"
-                          className="btn btn-danger"
+                          type="submit"
+                          className="btn btn-success"
                           onClick={() =>
-                            this.props.removeProductFromCart(product)
+                            this.addMultiple(product, this.state.selectValue)
                           }
                         >
-                          <FontAwesomeIcon icon="trash-alt" />
+                          <FontAwesomeIcon icon="cart-plus" />
                         </button>
-                      ) : (
-                        ""
-                      )}
-                    </div>
-                  )}
+                        {this.inCart() > 0 ? (
+                          <button
+                            type="button"
+                            className="btn btn-danger"
+                            onClick={() =>
+                              this.props.removeProductFromCart(product)
+                            }
+                          >
+                            <FontAwesomeIcon icon="trash-alt" />
+                          </button>
+                        ) : (
+                          ""
+                        )}
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      );
+        );
+      }
     }
   }
 }
