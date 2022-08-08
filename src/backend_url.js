ff3538518cd642b0424e206df569117d0c1003c7
Kamil Kosiba <kamil.kosiba@outlook.com>
2022-08-08 10:49:48 +0100

    Removed redundant comment

--- src/backend_url.js
+++ src/backend_url.js
@@ -1,2 +1,1 @@
-// export const API_PATH = "http://localhost:8000/api/";
-export const API_PATH = "https://ecommerce-backend-django.herokuapp.com/api/";
+export const API_PATH = "http://localhost:8000/api/";
